87082ab8bbcb4c629c1829dc253c163bf7be9274
Aliyarm <aliyar.m03@gmail.com>
2023-08-23 18:36:02 +0600

    login_form

--- lib/aliyar_web/controllers/aliyar_controller.ex
+++ lib/aliyar_web/controllers/aliyar_controller.ex
@@ -1,9 +1,12 @@
 defmodule AliyarWeb.AliyarController do
   use AliyarWeb, :controller
+  alias Aliyar.Context.Exchanges
 
   def index(conn, _params) do
+    exchanges = Exchanges.all
     conn
     |> put_layout(html: :page)
-    |> render(:index)
+    |> render(:index, exchanges: "Aliyar durak-prostak")
   end
+
 end
